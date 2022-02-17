2fb167af1c979dd8bdc585f151eeb3f10bb58b7c
Tammam Khan <tammam.a.khan@gmail.com>
2022-02-16 16:58:08 -0800

    added high score feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -247,25 +247,37 @@ var startGame = function () {
 
 // function to end the entire game
 var endGame = function () {
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
+  window.alert("The game has now ended. Let's see how you did!");
+
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
     alert(
-      "Great job, you've survived the game! You now have a score of " +
-        playerInfo.money +
-        "."
+      playerInfo.name + " now has the high score of " + playerInfo.money + "!"
     );
   } else {
-    alert("You've lost your robot in battle.");
+    alert(
+      playerInfo.name +
+        " did not beat the high score of " +
+        highScore +
+        ". Maybe next time!"
+    );
   }
 
   // ask player if they'd like to play again
-  var playAgainConfirm = confirm("Would you like to play again?");
+  var playAgainConfirm = window.confirm("Would you like to play again?");
 
   if (playAgainConfirm) {
-    // restart the game
     startGame();
   } else {
-    alert("Thank you for playing Robot Gladiators! Come back soon!");
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
   }
 };
 
